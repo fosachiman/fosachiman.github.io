7de8d3be2801ce73b15f798842bc1b02d149e32a
Jason Humphreys <jasonphumphreys@gmail.com>
2016-11-26 11:52:00 -0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//Updates what I'm doing currently
+var currently = document.getElementById("currently");
+currently.innerHTML = "enrolled in the Web Development Immersive program at General Assembly."
+
+//Updates the status of my projects
+var projects = document.getElementById("projects");
+projects.innerHTML = "Coming Soon!";
