797d210b2830a745b85a6ab705764048b625ef38
fosachiman <jasonphumphreys@gmail.com>
2017-03-08 18:02:41 -0500

    new site

--- Readme.md
+++
@@ -1,5 +0,0 @@
-This site was built using:
- - HTML5
- - CSS3
-    - @webkit animation & @font-face selectors
- - Javascript
--- javascript.js
+++
@@ -1,7 +0,0 @@
-//Updates what I'm doing currently
-var currently = document.getElementById("currently");
-currently.innerHTML = "enrolled in the Web Development Immersive program at General Assembly."
-
-//Updates the status of my projects
-// var projects = document.getElementById("projects");
-// projects.innerHTML = "Coming Soon!";
